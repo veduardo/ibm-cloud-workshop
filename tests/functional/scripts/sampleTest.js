076cec5987c1df1ce5a3f6fc71a8ce5774f1e8bc
Vinicius Eduardo Soares da Silva <vesoares@br.ibm.com>
2017-12-11 16:54:02 -0200

    Point tests to the application

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -1,11 +1,12 @@
 module.exports = {
   'Sample functional test' : function (browser) {
     browser
-      .url('http://google.com')
-      .waitForElementVisible('input.gsfi', 60000, true,
+      .url('http://ibm-cloud-workshop.mybluemix.net/index.html')
+      .waitForElementVisible('input.form-control', 60000, true,
         function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .setValue('input.form-control', 'vesoares@br.ibm.com')
+      .click('button[id=btn-subscribe]')
+      .waitForElementPresent('#feedback', 60000)
       .pause(5000)
       .end();
   }
